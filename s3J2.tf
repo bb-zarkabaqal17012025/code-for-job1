6269c619ef19b170e465d60ae8a0ac9daf0dff05
Zarka <zarka.baqal@bigbasket.com>
2025-04-01 13:09:48 +0530

    Initial commit with s3J3.tf

---
+++ s3J2.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "zarka-bucket-tf-123"  # Change to a globally unique name
+  acl    = "public-read"  # Options: private, public-read, public-read-write, authenticated-read
+
+  tags = {
+    Name  = "zarka-bucket-tf-123"
+    team  = "Cloudnauts"
+    Owner = "zarka.baqal@bigbasket.com"
+  }
+}
+
+
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
+}
