7e2fa6e00b5c64d2c0b40971f42aef559e9c66de
bb-zarkabaqal17012025 <zarka.baqal@bigbasket.com>
2025-04-01 13:25:51 +0530

    Delete s3J2.tf

--- s3J2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "zarka-bucket-tf-123" {
-  bucket = "zarka-bucket-tf-123"
-}
-
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.zarka-bucket-tf-123.id
-  acl    = "public-read"
-}
