fa91b743729aeaff768d0bfc84194fa8c188da0a
bb-zarkabaqal17012025 <zarka.baqal@bigbasket.com>
2025-04-01 13:13:56 +0530

    Update s3J2.tf

--- s3J2.tf
+++ s3J2.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "zarka-bucket-tf-123" {
   bucket = "zarka-bucket-tf-123"  # Change to a globally unique name
   acl    = "public-read"  # Options: private, public-read, public-read-write, authenticated-read
 
